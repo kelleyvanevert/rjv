d195fc98bf9a8883125dd02cb6547fa055f5f51f
Kelley van Evert <hello@klve.nl>
2023-06-30 19:28:01 +0200

    stuck

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,52 @@
-use js_sandbox::{AnyError, Script};
+use atomic_float::AtomicF32;
+use js_sandbox::Script;
 use nih_plug::prelude::*;
+use nih_plug_egui::{create_egui_editor, egui, widgets, EguiState};
 use std::sync::Arc;
 
 // This is a shortened version of the gain example with most comments removed, check out
 // https://github.com/robbert-vdh/nih-plug/blob/master/plugins/examples/gain/src/lib.rs to get
 // started
 
+/// The time it takes for the peak meter to decay by 12 dB after switching to complete silence.
+const PEAK_METER_DECAY_MS: f64 = 150.0;
+
 struct Rjv {
     params: Arc<RjvParams>,
     sample_rate: f32,
 
+    code: String,
     time_s: f32,
+
+    /// Needed to normalize the peak meter's response based on the sample rate.
+    peak_meter_decay_weight: f32,
+    /// The current data for the peak meter. This is stored as an [`Arc`] so we can share it between
+    /// the GUI and the audio processing parts. If you have more state to share, then it's a good
+    /// idea to put all of that in a struct behind a single `Arc`.
+    ///
+    /// This is stored as voltage gain.
+    peak_meter: Arc<AtomicF32>,
+}
+
+struct UIState {
+    code: String,
+}
+
+impl Default for UIState {
+    fn default() -> Self {
+        Self {
+            code: "Math.sin(t) * 20".to_string(),
+        }
+    }
 }
 
 #[derive(Params)]
 struct RjvParams {
+    /// The editor state, saved together with the parameter state so the custom scaling can be
+    /// restored.
+    #[persist = "editor-state"]
+    editor_state: Arc<EguiState>,
+
     /// The parameter's ID is used to identify the parameter in the wrappred plugin API. As long as
     /// these IDs remain constant, you can rename and reorder these fields as you wish. The
     /// parameters are exposed to the host in the same order they were defined. In this case, this
@@ -29,7 +61,11 @@ impl Default for Rjv {
             params: Arc::new(RjvParams::default()),
             sample_rate: 1.0,
 
+            code: "Math.sin(t) * 20".to_string(),
             time_s: 0.0,
+
+            peak_meter_decay_weight: 1.0,
+            peak_meter: Arc::new(AtomicF32::new(util::MINUS_INFINITY_DB)),
         }
     }
 }
@@ -37,6 +73,8 @@ impl Default for Rjv {
 impl Default for RjvParams {
     fn default() -> Self {
         Self {
+            editor_state: EguiState::from_size(400, 300),
+
             // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
             // to treat these kinds of parameters as if we were dealing with decibels. Storing this
             // as decibels is easier to work with, but requires a conversion for every sample.
@@ -61,12 +99,6 @@ impl Default for RjvParams {
             .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
             .with_string_to_value(formatters::s2v_f32_gain_to_db()),
         }
-    }
-}
-
-impl Rjv {
-    fn bla(&mut self) -> f32 {
-        0.0
     }
 }
 
@@ -111,12 +143,89 @@ impl Plugin for Rjv {
         self.params.clone()
     }
 
+    fn editor(&mut self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
+        let params = self.params.clone();
+        let peak_meter = self.peak_meter.clone();
+
+        create_egui_editor(
+            self.params.editor_state.clone(),
+            UIState::default(),
+            |_, _| {},
+            move |egui_ctx, setter, state| {
+                egui::CentralPanel::default().show(egui_ctx, |ui| {
+                    // NOTE: See `plugins/diopser/src/editor.rs` for an example using the generic UI widget
+
+                    // // This is a fancy widget that can get all the information it needs to properly
+                    // // display and modify the parameter from the parametr itself
+                    // // It's not yet fully implemented, as the text is missing.
+                    // ui.label("Some random integer");
+                    // ui.add(widgets::ParamSlider::for_param(&params.some_int, setter));
+
+                    ui.label("Gain");
+                    ui.add(widgets::ParamSlider::for_param(&params.gain, setter));
+
+                    ui.label(
+                        "Also gain, but with a lame widget. Can't even render the value correctly!",
+                    );
+                    // This is a simple naieve version of a parameter slider that's not aware of how
+                    // the parameters work
+                    ui.add(
+                        egui::widgets::Slider::from_get_set(-30.0..=30.0, |new_value| {
+                            match new_value {
+                                Some(new_value_db) => {
+                                    let new_value = util::gain_to_db(new_value_db as f32);
+
+                                    setter.begin_set_parameter(&params.gain);
+                                    setter.set_parameter(&params.gain, new_value);
+                                    setter.end_set_parameter(&params.gain);
+
+                                    new_value_db
+                                }
+                                None => util::gain_to_db(params.gain.value()) as f64,
+                            }
+                        })
+                        .suffix(" dB"),
+                    );
+
+                    ui.label("JavaScript code");
+                    let resp =
+                        ui.add(egui::widgets::TextEdit::multiline(&mut state.code).code_editor());
+                    if resp.changed() {
+                        // self.code = state.code.clone();
+                    }
+
+                    // TODO: Add a proper custom widget instead of reusing a progress bar
+                    let peak_meter =
+                        util::gain_to_db(peak_meter.load(std::sync::atomic::Ordering::Relaxed));
+                    let peak_meter_text = if peak_meter > util::MINUS_INFINITY_DB {
+                        format!("{peak_meter:.1} dBFS")
+                    } else {
+                        String::from("-inf dBFS")
+                    };
+
+                    let peak_meter_normalized = (peak_meter + 60.0) / 60.0;
+                    ui.allocate_space(egui::Vec2::splat(2.0));
+                    ui.add(
+                        egui::widgets::ProgressBar::new(peak_meter_normalized)
+                            .text(peak_meter_text),
+                    );
+                });
+            },
+        )
+    }
+
     fn initialize(
         &mut self,
         _audio_io_layout: &AudioIOLayout,
         buffer_config: &BufferConfig,
         _context: &mut impl InitContext<Self>,
     ) -> bool {
+        // After `PEAK_METER_DECAY_MS` milliseconds of pure silence, the peak meter's value should
+        // have dropped by 12 dB
+        self.peak_meter_decay_weight = 0.25f64
+            .powf((buffer_config.sample_rate as f64 * PEAK_METER_DECAY_MS / 1000.0).recip())
+            as f32;
+
         self.sample_rate = buffer_config.sample_rate;
 
         // Resize buffers and perform other potentially expensive initialization operations here.
@@ -136,18 +245,39 @@ impl Plugin for Rjv {
         _aux: &mut AuxiliaryBuffers,
         _context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
-        let js_code = "function bla([t, g]) { return Math.sin(t) * g; }";
-        let mut script = Script::from_string(js_code).expect("JS compiles");
+        let js_code = format!("function gain(t) {{ return {}; }}", self.code);
+        // let js_code = "function bla([t, g]) { return Math.sin(t) * g; }";
+        let mut script = Script::from_string(&js_code).expect("JS compiles");
 
         for (sample_id, channel_samples) in buffer.iter_samples().enumerate() {
             let time = self.time_s + ((sample_id as f32) / self.sample_rate);
 
+            let mut amplitude = 0.0;
+            let num_samples = channel_samples.len();
+
             // Smoothing is optionally built into the parameters themselves
-            let gain = self.params.gain.smoothed.next();
-            let gain_processed: f32 = script.call("bla", &[time, gain]).expect("JS runs");
+            // let gain = self.params.gain.smoothed.next();
+            let gain_processed: f32 = script.call("gain", &time).expect("JS runs");
 
             for sample in channel_samples {
                 *sample *= gain_processed;
+                amplitude += *sample;
+            }
+
+            // To save resources, a plugin can (and probably should!) only perform expensive
+            // calculations that are only displayed on the GUI while the GUI is open
+            if self.params.editor_state.is_open() {
+                amplitude = (amplitude / num_samples as f32).abs();
+                let current_peak_meter = self.peak_meter.load(std::sync::atomic::Ordering::Relaxed);
+                let new_peak_meter = if amplitude > current_peak_meter {
+                    amplitude
+                } else {
+                    current_peak_meter * self.peak_meter_decay_weight
+                        + amplitude * (1.0 - self.peak_meter_decay_weight)
+                };
+
+                self.peak_meter
+                    .store(new_peak_meter, std::sync::atomic::Ordering::Relaxed)
             }
         }
 
