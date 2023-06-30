4aa3938b2a56348c5e67c592953f1534e505eeec
Kelley van Evert <hello@klve.nl>
2023-06-30 17:43:08 +0200

    Woo! JS is running inside a Rust-based VST

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rjv
+
+## Building
+
+After installing [Rust](https://rustup.rs/), you can compile Rjv as follows:
+
+```shell
+cargo xtask bundle rjv --release
+```
---
+++ src/lib.rs
@@ -0,0 +1,179 @@
+use js_sandbox::{AnyError, Script};
+use nih_plug::prelude::*;
+use std::sync::Arc;
+
+// This is a shortened version of the gain example with most comments removed, check out
+// https://github.com/robbert-vdh/nih-plug/blob/master/plugins/examples/gain/src/lib.rs to get
+// started
+
+struct Rjv {
+    params: Arc<RjvParams>,
+    sample_rate: f32,
+
+    time_s: f32,
+}
+
+#[derive(Params)]
+struct RjvParams {
+    /// The parameter's ID is used to identify the parameter in the wrappred plugin API. As long as
+    /// these IDs remain constant, you can rename and reorder these fields as you wish. The
+    /// parameters are exposed to the host in the same order they were defined. In this case, this
+    /// gain parameter is stored as linear gain while the values are displayed in decibels.
+    #[id = "gain"]
+    pub gain: FloatParam,
+}
+
+impl Default for Rjv {
+    fn default() -> Self {
+        Self {
+            params: Arc::new(RjvParams::default()),
+            sample_rate: 1.0,
+
+            time_s: 0.0,
+        }
+    }
+}
+
+impl Default for RjvParams {
+    fn default() -> Self {
+        Self {
+            // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
+            // to treat these kinds of parameters as if we were dealing with decibels. Storing this
+            // as decibels is easier to work with, but requires a conversion for every sample.
+            gain: FloatParam::new(
+                "Mancala",
+                util::db_to_gain(0.0),
+                FloatRange::Skewed {
+                    min: util::db_to_gain(-30.0),
+                    max: util::db_to_gain(30.0),
+                    // This makes the range appear as if it was linear when displaying the values as
+                    // decibels
+                    factor: FloatRange::gain_skew_factor(-30.0, 30.0),
+                },
+            )
+            // Because the gain parameter is stored as linear gain instead of storing the value as
+            // decibels, we need logarithmic smoothing
+            .with_smoother(SmoothingStyle::Logarithmic(50.0))
+            .with_unit(" dB")
+            // There are many predefined formatters we can use here. If the gain was stored as
+            // decibels instead of as a linear gain value, we could have also used the
+            // `.with_step_size(0.1)` function to get internal rounding.
+            .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
+            .with_string_to_value(formatters::s2v_f32_gain_to_db()),
+        }
+    }
+}
+
+impl Rjv {
+    fn bla(&mut self) -> f32 {
+        0.0
+    }
+}
+
+impl Plugin for Rjv {
+    const NAME: &'static str = "Rjv";
+    const VENDOR: &'static str = "Kelley van Evert";
+    const URL: &'static str = env!("CARGO_PKG_HOMEPAGE");
+    const EMAIL: &'static str = "hello@klve.nl";
+
+    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+    // The first audio IO layout is used as the default. The other layouts may be selected either
+    // explicitly or automatically by the host or the user depending on the plugin API/backend.
+    const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[AudioIOLayout {
+        main_input_channels: NonZeroU32::new(2),
+        main_output_channels: NonZeroU32::new(2),
+
+        aux_input_ports: &[],
+        aux_output_ports: &[],
+
+        // Individual ports and the layout as a whole can be named here. By default these names
+        // are generated as needed. This layout will be called 'Stereo', while a layout with
+        // only one input and output channel would be called 'Mono'.
+        names: PortNames::const_default(),
+    }];
+
+    const MIDI_INPUT: MidiConfig = MidiConfig::None;
+    const MIDI_OUTPUT: MidiConfig = MidiConfig::None;
+
+    const SAMPLE_ACCURATE_AUTOMATION: bool = true;
+
+    // If the plugin can send or receive SysEx messages, it can define a type to wrap around those
+    // messages here. The type implements the `SysExMessage` trait, which allows conversion to and
+    // from plain byte buffers.
+    type SysExMessage = ();
+    // More advanced plugins can use this to run expensive background tasks. See the field's
+    // documentation for more information. `()` means that the plugin does not have any background
+    // tasks.
+    type BackgroundTask = ();
+
+    fn params(&self) -> Arc<dyn Params> {
+        self.params.clone()
+    }
+
+    fn initialize(
+        &mut self,
+        _audio_io_layout: &AudioIOLayout,
+        buffer_config: &BufferConfig,
+        _context: &mut impl InitContext<Self>,
+    ) -> bool {
+        self.sample_rate = buffer_config.sample_rate;
+
+        // Resize buffers and perform other potentially expensive initialization operations here.
+        // The `reset()` function is always called right after this function. You can remove this
+        // function if you do not need it.
+        true
+    }
+
+    fn reset(&mut self) {
+        // Reset buffers and envelopes here. This can be called from the audio thread and may not
+        // allocate. You can remove this function if you do not need it.
+    }
+
+    fn process(
+        &mut self,
+        buffer: &mut Buffer,
+        _aux: &mut AuxiliaryBuffers,
+        _context: &mut impl ProcessContext<Self>,
+    ) -> ProcessStatus {
+        let js_code = "function bla([t, g]) { return Math.sin(t) * g; }";
+        let mut script = Script::from_string(js_code).expect("JS compiles");
+
+        for (sample_id, channel_samples) in buffer.iter_samples().enumerate() {
+            let time = self.time_s + ((sample_id as f32) / self.sample_rate);
+
+            // Smoothing is optionally built into the parameters themselves
+            let gain = self.params.gain.smoothed.next();
+            let gain_processed: f32 = script.call("bla", &[time, gain]).expect("JS runs");
+
+            for sample in channel_samples {
+                *sample *= gain_processed;
+            }
+        }
+
+        self.time_s += (buffer.samples() as f32) / self.sample_rate;
+
+        ProcessStatus::Normal
+    }
+}
+
+impl ClapPlugin for Rjv {
+    const CLAP_ID: &'static str = "nl.klve.rjv";
+    const CLAP_DESCRIPTION: Option<&'static str> = Some("Rust, JS, VST");
+    const CLAP_MANUAL_URL: Option<&'static str> = Some(Self::URL);
+    const CLAP_SUPPORT_URL: Option<&'static str> = None;
+
+    // Don't forget to change these features
+    const CLAP_FEATURES: &'static [ClapFeature] = &[ClapFeature::AudioEffect, ClapFeature::Stereo];
+}
+
+impl Vst3Plugin for Rjv {
+    const VST3_CLASS_ID: [u8; 16] = *b"rjv_klve_1234567";
+
+    // And also don't forget to change these categories
+    const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] =
+        &[Vst3SubCategory::Fx, Vst3SubCategory::Dynamics];
+}
+
+nih_export_clap!(Rjv);
+nih_export_vst3!(Rjv);
